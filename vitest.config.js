dfef6d87dc8d6df0cb0d5185b8dd6a94e01121e4
sinedied <noda@free.fr>
2025-07-10 11:48:29 +0200

    test: setup vitest

---
+++ vitest.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      reporter: ['text'],
+      exclude: [
+        'node_modules/',
+        'test/',
+        '**/*.config.js'
+      ]
+    }
+  }
+});
